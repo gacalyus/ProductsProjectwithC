998f4c13554fd011c46fdc50b695921d82bc9e69
gacalyus <83091221+gacalyus@users.noreply.github.com>
2024-08-12 16:02:08 +0300

    Validationtools and attribuite

--- Business/Concrete/ProductManager.cs
+++ Business/Concrete/ProductManager.cs
@@ -1,9 +1,13 @@
 ﻿using Business.Abstract;
 using Business.Constans;
+using Business.ValidationRules.FluentValidation;
+using Core.Aspects.Autofac.Validation;
+using Core.CrossCuttingConcerns.Validation;
 using Core.Utilities.Results;
 using DataAccess.Abstract;
 using Entities.Concrete;
 using Entities.DTOs;
+using FluentValidation;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -21,12 +25,11 @@ namespace Business.Concrete
             _productDal = productDal;
         }
 
+        [ValidationAspect(typeof(ProductValidator))]
         public IResult Add(Product product)
         {
-            if (product.ProductName.Length == 22)
-            {
-                return new ErrorMessage(Messages.ProductNameInValid);
-            }
+
+
             _productDal.Add(product);
             return new SuccessResult(Messages.ProductAdded);
         }
